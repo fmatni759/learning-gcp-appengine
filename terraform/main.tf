7bbf78d8a9ba6aa2b3778d86ba8dc669ad8ca6d7
fmatni759 <felmatni@gmail.com>
2026-03-07 12:39:34 -0500

    fix: retirer App Engine resource de Terraform - deja cree

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,3 @@ resource "google_project_service" "cloudbuild" {
   service            = "cloudbuild.googleapis.com"
   disable_on_destroy = false
 }
-
-# App Engine application
-resource "google_app_engine_application" "app" {
-  project     = var.project_id
-  location_id = var.region
-
-  depends_on = [google_project_service.appengine]
-}
